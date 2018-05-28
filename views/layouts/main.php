e53eaa7025f59b3f843dfcd164af6eb912bd10d6
MroiDev <delawyers@gmail.com>
2018-05-28 14:06:00 +0300

    Change template

---
+++ views/layouts/main.php
@@ -0,0 +1,72 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="About the page">
+
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+
+<body>
+<?php $this->beginBody() ?>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Vehicles Store</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Link 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link 2</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="#">Link 1 (will be implemented later)</a>
+                    <a class="dropdown-item" href="#">Link 2 (will be implemented later)</a>
+                    <a class="dropdown-item" href="#">Link 3 (will be implemented later)</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<!--/ Navbar -->
+
+<main role="main" class="container">
+    <?= $content ?>
+</main>
+
+<hr>
+<footer class="container">
+    <p>&copy; Vehicles Store :: Online Store, 2018</p>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ views/site/index.php
@@ -0,0 +1,43 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="web/img/default.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Vehicle title</h5>
+                    <p class="card-text">Some quick example text to build on the Vehicle title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-success">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="web/img/default.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Vehicle title</h5>
+                    <p class="card-text">Some quick example text to build on the Vehicle title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-success">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="web/img/default.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Vehicle title</h5>
+                    <p class="card-text">Some quick example text to build on the Vehicle title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-success">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
